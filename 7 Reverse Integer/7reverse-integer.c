e1afbdd00ff03ea08b82225bb16d919941280e3e
Sarthak <sarthakkothiyal83@gmail.com>
2024-10-14 13:21:25 +0000

    Code challenge submission for question: 7 Reverse Integer

---
+++ 7 Reverse Integer/7reverse-integer.c
@@ -0,0 +1,16 @@
+#include <limits.h>
+int reverse(int x){
+    int num=0;
+    while(x!=0){
+        int temp=x%10;
+        x/=10;
+        if(num>(INT_MAX/10) || num==INT_MAX/10 && temp>7){
+            return 0;
+        }
+        if(num<(INT_MIN/10) || num==INT_MIN/10 && temp<-8){
+            return 0;
+        }
+        num=num*10+temp;
+    }
+    return num;
+}
