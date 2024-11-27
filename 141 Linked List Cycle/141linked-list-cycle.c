230cf07f15e89bf20ec8be01ffc2ae3d78063ab2
Sarthak <sarthakkothiyal83@gmail.com>
2024-11-27 04:04:25 +0000

    Code challenge submission for question: 141 Linked List Cycle

---
+++ 141 Linked List Cycle/141linked-list-cycle.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+typedef struct ListNode node;
+bool hasCycle(struct ListNode *head) {
+    if(head==NULL)
+        return false;
+    node *slow=head,*fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(fast==slow){
+            return true;
+        }
+    }
+    return false;
+}
