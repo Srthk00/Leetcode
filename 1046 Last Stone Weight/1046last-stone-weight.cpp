6ddef4c9e6ad00f47a76c09de07bd2657f549cd2
Sarthak <sarthakkothiyal83@gmail.com>
2025-03-30 14:36:21 +0000

    Code challenge submission for question: 1046 Last Stone Weight

---
+++ 1046 Last Stone Weight/1046last-stone-weight.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int i:stones){
+            pq.push(i);
+        }
+        while(pq.size()>1){
+            int y=pq.top();
+            pq.pop();
+            int x=pq.top();
+            pq.pop();
+            if(x==y){
+                continue;
+            }
+            else{
+                int result=y-x;
+                pq.push(result);
+            }
+        }
+        if(pq.empty())
+            return 0;
+        return pq.top();
+    }
+};
