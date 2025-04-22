db7b97aae3b1157ccba1268794271bef2f8747e5
Sarthak <sarthakkothiyal83@gmail.com>
2025-04-22 17:20:20 +0000

    Code challenge submission for question: 881 Boats to Save People

---
+++ 881 Boats to Save People/881boats-to-save-people.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int low=0,high=people.size()-1;
+        int count=0;
+        while(low<=high){
+            if(people[low]+people[high]<=limit){
+                low++;
+            }
+            high--;
+            count++;
+        }
+        return count;
+    }
+};
