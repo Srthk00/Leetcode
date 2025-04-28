379a663060e8f9ae5ba54821737f2c2ccb2c8c1b
Sarthak <sarthakkothiyal83@gmail.com>
2025-04-28 04:16:50 +0000

    Code challenge submission for question: 456 132 Pattern

---
+++ 456 132 Pattern/456132-pattern.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int second=INT_MIN;
+        stack<int> st;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(nums[i]<second){
+                return true;
+            }
+            while(!st.empty() && nums[st.top()]<nums[i]){
+                second=nums[st.top()];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return false;
+    }
+};
