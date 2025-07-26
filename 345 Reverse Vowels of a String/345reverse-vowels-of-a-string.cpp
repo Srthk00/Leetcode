caf6affe7bf184356b86dd140be9e1ab1a121363
Sarthak <sarthakkothiyal83@gmail.com>
2025-07-26 16:12:58 +0000

    Code challenge submission for question: 345 Reverse Vowels of a String

---
+++ 345 Reverse Vowels of a String/345reverse-vowels-of-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        string vowels="aeiouAEIOU";
+        int i=0,j=s.size()-1;
+        while(i<j){
+            while(i<s.size() && vowels.find(s[i])==string::npos){
+                i++;
+            }
+            while(j>=0 && vowels.find(s[j])==string::npos){
+                j--;
+            }
+            if(i<j){
+                swap(s[i++],s[j--]);
+            }
+        }
+        return s;
+    }
+};
