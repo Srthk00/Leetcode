fd97e6628fb0d69f0829f8ca9814614d4fc69e98
Sarthak <sarthakkothiyal83@gmail.com>
2025-11-15 17:34:17 +0000

    Code challenge submission for question: 3131 Find the Integer Added to Array I

---
+++ 3131 Find the Integer Added to Array I/3131find-the-integer-added-to-array-i.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+        int m1=*min_element(nums1.begin(),nums1.end());
+        int m2=*min_element(nums2.begin(),nums2.end());
+        return m2-m1;
+    }
+};
