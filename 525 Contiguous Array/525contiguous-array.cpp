d49b56ec159127c448d87d49c2463a5559e7c6bb
Sarthak <sarthakkothiyal83@gmail.com>
2026-01-17 18:03:22 +0000

    Code challenge submission for question: 525 Contiguous Array

---
+++ 525 Contiguous Array/525contiguous-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        int sum=0,result=0;
+        mp[0]=-1;
+        for(int i=0;i<nums.size();i++){
+            sum+=(nums[i]==1?1:-1);
+            if(mp.count(sum)){
+                result=max(result,i-mp[sum]);
+            }
+            else{
+                mp[sum]=i;
+            }
+        }
+        return result;
+    }
+};
