65b6a59024064955b599a6758fc045d0b7410683
Sarthak Kothiyal <sarthakkothiyal83@gmail.com>
2026-07-25 12:52:20 +0530

    Time: 0 ms (100%), Space: 30.9 MB (39.98%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        bool changed=false;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]<=nums[i+1]){
+                continue;
+            }
+            else{
+                if(changed){
+                    return false;
+                }
+                else{
+                    if(i==0 || nums[i-1]<=nums[i+1]){
+                        nums[i]=nums[i+1];
+                    }
+                    else{
+                        nums[i+1]=nums[i];
+                    }
+                    changed=true;
+                }
+            }
+        }
+        return true;
+    }
+};
