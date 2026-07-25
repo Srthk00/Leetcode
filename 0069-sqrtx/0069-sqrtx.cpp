abefcc02e63165337a97d5de1788567c489ed817
Sarthak Kothiyal <sarthakkothiyal83@gmail.com>
2026-07-26 01:39:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=0,high=x/2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long long sq=(long long)mid*mid;
+            if(sq==x){
+                return mid;
+            }
+            else if(sq<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return high;
+    }
+};
