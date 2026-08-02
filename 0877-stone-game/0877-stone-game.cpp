09d0a95e0123c2b2301a9218f88c34dcc7988491
Sarthak Kothiyal <sarthakkothiyal83@gmail.com>
2026-08-02 13:35:40 +0530

    Time: 9 ms (29.89%), Space: 19.8 MB (11.06%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+
+    int solve(int i,int j,vector<int> &nums){
+        if(i>j){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int take_left=nums[i]+min(solve(i+2,j,nums),solve(i+1,j-1,nums));
+        int take_right=nums[j]+min(solve(i+1,j-1,nums),solve(i,j-2,nums));
+        return dp[i][j]=max(take_left,take_right);
+    }
+
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int sum=accumulate(piles.begin(),piles.end(),0);
+        dp=vector<vector<int>>(piles.size(),vector<int>(piles.size(),-1));
+        return (solve(0,piles.size()-1,piles)*2)>sum;
     }
 };
